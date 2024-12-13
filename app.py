b10dc93d96fe69078a7ec56e11a57afd3cdac58a
saifulaji20 <73286300+saiful2030@users.noreply.github.com>
2024-12-13 07:27:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1969,6 +1969,7 @@ def logout():
     return redirect("/")
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
+
 
