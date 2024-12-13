78e1a85731337ef9ac01de94b5058bc7b4971e88
saifulaji20 <73286300+saiful2030@users.noreply.github.com>
2024-12-13 12:37:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://127.0.0.1:5000/callback"
+    redirect_uri="https://takehome6-bdbsasbwebcwh3hn.canadacentral-01.azurewebsites.net/callback"
 
 )
 
